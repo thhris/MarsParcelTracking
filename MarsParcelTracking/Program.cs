8c9b4f69072982de2dd0d9141cdb65f1cf40dcad
Teodor Hristoforov <teodor.hristoforov@royalmail.com>
2025-09-12 20:27:43 +0100

    Added Swagger UI for development and configured it to open on start

--- MarsParcelTracking/Program.cs
+++ MarsParcelTracking/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -19,5 +21,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Add Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
