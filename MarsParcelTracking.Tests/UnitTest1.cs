9ea416b11dd88fd0dfba16781f30eaf4504001ae
Teodor Hristoforov <teodor.hristoforov@royalmail.com>
2025-09-12 20:29:06 +0100

    Created a default test project

---
+++ MarsParcelTracking.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsParcelTracking.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
